5ef3716c65479d6e9e163206f118b45ee5e951c0
hcg0127 <115148838+hcg0127@users.noreply.github.com>
2023-01-31 21:50:40 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -7,11 +7,13 @@ int main() {
   cin.tie(0);
   cout.tie(0);
 
-  int l,h=0;
+  int l;
+  long long h=0, r=1;
   string x;
   cin >> l >> x;
   for (int i=0; i<l; i++) {
-    h += (x[i]-96) * (int)pow(31,i);
+    h += (x[i]-96) * r % 1234567891;
+    r = (r * 31) % 1234567891;
   }
-  cout << h % 1234567891;
+  cout << h;
 }
