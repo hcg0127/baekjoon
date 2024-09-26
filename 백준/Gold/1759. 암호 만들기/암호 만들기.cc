851c4c36e104b30c44de90f70a95611f5cd016ba
hcg0127 <115148838+hcg0127@users.noreply.github.com>
2024-09-26 12:23:30 +0900

    [Gold V] Title: 암호 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int l,c;
+bool visit[26];
+vector<char> v;
+
+void dfs(int dep, int idx, int a, int b, string tmp) {
+    if (dep==l) {
+        bool down = false;
+        if (a>=1 && b>=2) {
+            cout << tmp << '\n';
+        }
+        return;
+    }
+
+    for (int i=idx; i<c; i++) {
+        char ch = v[i];
+        if (!visit[ch-'a']) {
+            visit[ch-'a'] = true;
+            if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') {
+                dfs(dep+1,i+1,a+1,b,tmp+ch);
+            }
+            else {
+                dfs(dep+1,i+1,a,b+1,tmp+ch);
+            }
+            visit[ch-'a'] = false;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> l >> c;
+    char ch;
+    for (int i=0; i<c; i++) {
+        cin >> ch;
+        v.push_back(ch);
+    }
+    sort(v.begin(),v.end());
+
+    dfs(0,0,0,0,"");
+}
