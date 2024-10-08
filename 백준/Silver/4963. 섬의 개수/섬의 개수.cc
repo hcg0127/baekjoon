2e37a64c935bea982ab318c4ce12112e4f78f8b4
hcg0127 <115148838+hcg0127@users.noreply.github.com>
2024-10-08 15:22:38 +0900

    [Silver II] Title: 섬의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.cc
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.cc
@@ -1,51 +1,49 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <stack>
+#include <queue>
+#define pii pair<int,int>
 using namespace std;
 
-int n,m,k,x,y,visit[50][50],edge[50][50];
-int dx[8]={0,0,1,-1,1,1,-1,-1},dy[8]={1,-1,0,0,1,-1,1,-1};
+int w,h,dx[8]={0,0,-1,-1,-1,1,1,1},dy[8]={-1,1,-1,0,1,-1,0,1};
+bool board[50][50],visit[50][50];
 
-void dfs(int a, int b) {
-  visit[a][b] = 1;
-  for (int i=0; i<8; i++) {
-    int cx = a+dx[i], cy = b+dy[i];
-    if (cx<0 || cy<0 || cx>=n || cy>=m)
-      continue;
-    if (visit[cx][cy]==0 && edge[cx][cy]==1)
-      dfs(cx,cy);
-  }
+void bfs(int x, int y) {
+    queue<pii> q;
+    q.push({x,y});
+    visit[x][y] = true;
+    while (!q.empty()) {
+        auto [a,b] = q.front();
+        q.pop();
+        for (int i=0; i<8; i++) {
+            int cx=a+dx[i],cy=b+dy[i];
+            if (cx<0 || cy<0 || cx>=w || cy>=h || visit[cx][cy] || !board[cx][cy]) continue;
+            q.push({cx,cy});
+            visit[cx][cy] = true;
+        }
+    }
 }
 
 int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
 
-  while (1) {
-    cin >> m >> n;
-    if (n == 0 && m == 0) break;
-    int cnt=0;
-    for (int r=0; r<50; r++) {
-      for (int c=0; c<50; c++) {
-        visit[r][c]=0;
-        edge[r][c]=0;
-      }
-    }
-    for (int i=0; i<n; i++) {
-      for (int j=0; j<m; j++) {
-        cin >> edge[i][j];
-      }
-    }
-    for (int i=0; i<n; i++) {
-      for (int j=0; j<m; j++) {
-        if (edge[i][j]==1&&visit[i][j]==0) {
-          cnt++;
-          dfs(i,j);
+    while (1) {
+        cin >> h >> w;
+        if (w==0 && h==0) break;
+        fill(&board[0][0],&board[50][0],0);
+        for (int i=0; i<w; i++)
+            for (int j=0; j<h; j++) cin >> board[i][j];
+        
+        fill(&visit[0][0],&visit[50][0],0);
+        int ans = 0;
+        for (int i=0; i<w; i++) {
+            for (int j=0; j<h; j++) {
+                if (board[i][j] && !visit[i][j]) {
+                    bfs(i,j);
+                    ans++;
+                }
+            }
         }
-      }
+        
+        cout << ans << '\n';
     }
-    cout << cnt << '\n';
-  }
 }
