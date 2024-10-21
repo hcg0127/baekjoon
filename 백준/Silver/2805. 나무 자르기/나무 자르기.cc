8ad42ca21a5cdb34a55286169f970bee4318a6f5
hcg0127 <115148838+hcg0127@users.noreply.github.com>
2024-10-21 15:34:41 +0900

    [Silver II] Title: 나무 자르기, Time: 144 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -2,17 +2,19 @@
 #include <algorithm>
 using namespace std;
 
-int n,m,tr[1000001];
+int n,m,tr[1000001],mx;
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
     cin >> n >> m;
-    for (int i=0; i<n; i++) cin >>tr[i];
-    sort(tr,tr+n);
+    for (int i=0; i<n; i++) {
+        cin >>tr[i];
+        mx = max(tr[i],mx);
+    }
 
-    long long int l=0,r=tr[n-1],ans=0;
+    long long int l=0,r=mx,ans=0;
     while (l<=r) {
         long long int mid = (l+r)/2,sum=0;
         for (int i=0; i<n; i++) {
