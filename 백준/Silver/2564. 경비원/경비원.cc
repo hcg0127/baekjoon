87adc6fb177c6e88a7f7bdcd7d82e1b85218f5c7
hcg0127 <115148838+hcg0127@users.noreply.github.com>
2024-10-26 13:49:52 +0900

    [Silver I] Title: 경비원, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2564. 경비원/경비원.cc
+++ 백준/Silver/2564. 경비원/경비원.cc
@@ -5,6 +5,13 @@ using namespace std;
 int x,y,n,a,d,dg,ans;
 vector<int> v;
 
+int dton(int dir, int num) {
+    if (d==1) return num;
+    else if (d==4) return num+x;
+    else if (d==2) return x-num+x+y;
+    else return y-a+2*x+y;
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
@@ -12,16 +19,10 @@ int main() {
     cin >> x >> y >> n;
     for (int i=0; i<n; i++) {
         cin >> d >> a;
-        if (d==1) v.push_back(a);
-        else if (d==4) v.push_back(a+x);
-        else if (d==2) v.push_back(x-a+x+y);
-        else v.push_back(y-a+2*x+y);
+        v.push_back(dton(d,a));
     }
     cin >> d >> a;
-    if (d==1) dg = a;
-    else if (d==4) dg = a+x;
-    else if (d==2) dg = x-a+x+y;
-    else dg = y-a+2*x+y;
+    dg = dton(d,a);
 
     for (int i=0; i<n; i++) {
         int tmp = abs(v[i]-dg);
